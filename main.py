7cbdb2dcb011eb213467a73fc927ba33d8cfc122
astravital16 <dorianyork16@gmail.com>
2025-10-22 02:40:06 +0000

    B: set up player logic & groups

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        dt = clock.tick(60) / 1000
+        updatable.update(dt)
+
         screen.fill("black")
+        
+        for drawn_object in drawable:
+            drawn_object.draw(screen)
+
         pygame.display.flip()
 
 if __name__ == "__main__":
